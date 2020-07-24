d8b0b9627b9213b729171fac87c4222b1a3d3dd9
jingdd-boop <1736929925@qq.com>
2020-07-24 10:04:03 +0800

    js

---
+++ src/views/swiper/list.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   swiper
+ </div>
+</template>
+
+<script>
+import {fetchList} from '@/api/swiper'
+export default {
+  created(){
+    this.getList()
+  },
+  methods: {
+    getList(){
+      fetchList().then((res) =>{
+        console.log(res)
+      })
+    }
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
